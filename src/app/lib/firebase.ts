6dde4ac6d941b001517292fc948eb998c8cdb1ac
modernscape <modernscape@gmail.com>
2025-10-09 22:01:55 +0900

    25-10-09_01

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDOTb4EJ5ALjZuulL-AfZSy-Awi4g2vRYw",
-  authDomain: "next-things-app-9d94b.firebaseapp.com",
-  projectId: "next-things-app-9d94b",
-  storageBucket: "next-things-app-9d94b.firebasestorage.app",
-  messagingSenderId: "1056548789659",
-  appId: "1:1056548789659:web:4364c8981a3c925377953e",
-  measurementId: "G-431N4Q8HZ6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // 初期化
