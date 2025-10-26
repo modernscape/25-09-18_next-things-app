ae3e59457acec85c5c5c2a0c91b9b857e1e9962d
modernscape <modernscape@gmail.com>
2025-10-26 11:50:20 +0900

    25-10-26_

--- src/app/lib/firestore.ts
+++ src/app/lib/firestore.ts
@@ -15,12 +15,12 @@ import {
 import { db } from "./firebase";
 import { Thing } from "@/types";
 
-const KEY_THINGS = "things";
+const key_things = "things";
 
 // Thing
 // Create
 export async function addThing(title: string, order: number) {
-  return await addDoc(collection(db, KEY_THINGS), {
+  return await addDoc(collection(db, key_things), {
     title,
     items: [],
     trashed: false,
@@ -31,7 +31,7 @@ export async function addThing(title: string, order: number) {
 
 // Read (購読)
 export function subscribeThings(callback: (things: Thing[]) => void) {
-  const q = query(collection(db, KEY_THINGS), orderBy("order", "asc"));
+  const q = query(collection(db, key_things), orderBy("order", "asc"));
   return onSnapshot(q, (snapshot) => {
     const data = snapshot.docs.map((docSnap) => ({ id: docSnap.id, ...docSnap.data() } as Thing));
     callback(data);
@@ -40,20 +40,20 @@ export function subscribeThings(callback: (things: Thing[]) => void) {
 
 // Update
 export async function updateThing(id: string, newData: Partial<Thing>) {
-  const ref = doc(db, KEY_THINGS, id);
+  const ref = doc(db, key_things, id);
   await updateDoc(ref, newData);
 }
 
 // Delete
 export async function deleteThing(id: string) {
-  const ref = doc(db, KEY_THINGS, id);
+  const ref = doc(db, key_things, id);
   await deleteDoc(ref);
 }
 
 // Item
 // Create
 export async function addItem(thingID: string, text: string = "New Item") {
-  const ref = doc(db, KEY_THINGS, thingID);
+  const ref = doc(db, key_things, thingID);
   const newItem = { id: Date.now().toString(), text };
 
   await updateDoc(ref, {
@@ -62,34 +62,29 @@ export async function addItem(thingID: string, text: string = "New Item") {
 }
 
 // Update
-export async function updateItem(thingID: string, itemID: string, newTitle: string) {
-  const ref = doc(db, KEY_THINGS, thingID);
-  const snapshot = await getDoc(ref);
+export async function updateItem(thingID: string, itemID: string, newText: string) {
+  const thingRef = doc(db, key_things, thingID); // thing
+  const snapshot = await getDoc(thingRef); // thing
   if (!snapshot.exists()) return;
 
-  const thing = snapshot.data() as Thing;
-  const oldItem = thing.items.find((item) => item.id === itemID);
+  const thing = snapshot.data() as Thing; // thing
+  const oldItem = thing.items.find((item) => item.id === itemID); // item
   if (!oldItem) return;
 
-  if (newTitle === "") {
-    await updateDoc(ref, {
-      items: arrayRemove(oldItem),
-    });
-  } else {
-    const newItem = { ...oldItem, text: newTitle };
+  updateDoc(thingRef, {
+    items: arrayRemove(oldItem),
+  });
 
-    await updateDoc(ref, {
-      items: arrayRemove(oldItem),
-    });
-    await updateDoc(ref, {
-      items: arrayUnion(newItem),
+  if (newText !== "") {
+    await updateDoc(thingRef, {
+      items: arrayUnion({ ...oldItem, text: newText }),
     });
   }
 }
 
 // moveUp, moveUDown
 export async function moveThing(thingID: string, up: boolean) {
-  const q = query(collection(db, KEY_THINGS), orderBy("order", "asc"));
+  const q = query(collection(db, key_things), orderBy("order", "asc"));
   const snapshot = await getDocs(q);
   const things = snapshot.docs.map((doc) => ({
     id: doc.id,
@@ -98,14 +93,14 @@ export async function moveThing(thingID: string, up: boolean) {
 
   const index = things.findIndex((t) => t.id === thingID);
   const current = things[index];
-  const currentRef = doc(db, KEY_THINGS, current.id);
+  const currentRef = doc(db, key_things, current.id);
 
   console.log(index);
 
   if (up) {
     if (index <= 0) return;
     const prev = things[index - 1];
-    const prevRef = doc(db, KEY_THINGS, prev.id);
+    const prevRef = doc(db, key_things, prev.id);
 
     await Promise.all([
       updateDoc(currentRef, { order: prev.order }), //
@@ -114,7 +109,7 @@ export async function moveThing(thingID: string, up: boolean) {
   } else {
     if (index >= things.length - 1) return;
     const next = things[index + 1];
-    const nextRef = doc(db, KEY_THINGS, next.id);
+    const nextRef = doc(db, key_things, next.id);
 
     await Promise.all([
       updateDoc(currentRef, { order: next.order }), //
